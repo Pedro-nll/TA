a251d34653faa33c3a75c95bcdd6ed4e7a5fe730
Pedro Lambert <pedronll@outlook.com>
2023-06-30 13:28:45 -0300

    !

--- Algoritmos e Estruturas de Dados I/010-Trabalho final AED I/main.c
+++ Algoritmos e Estruturas de Dados I/010-Trabalho final AED I/main.c
@@ -28,6 +28,7 @@ int buscarPassageiroPorCPF(Passageiro *listaDePassageiros, int quantidadeDePassa
 int mostrarListaDeEspera(Passageiro *listaDePassageiros, int quantidadeDePassageiros);
 int cadastrarPassageiro(Passageiro *listaDePassageiros, int *quantidadeDePassageiros, int vooSelecionado);
 int excluirPassageiro(Passageiro *listaDePassageiros, int *quantidadeDePassageiros, int vooSelecionado);
+int validarCPF(const char *cpf);
 
 int main()
 {
@@ -35,7 +36,19 @@ int main()
     return 0;
 }
 
-//return 0 - para fechar o programa // sem codigo de erro pq ta num while loop
+/* ******************************
+Função: menuInicial
+Descrição:
+    Exibe o menu inicial, permitindo ao usuário selecionar um voo.
+    O usuário pode escolher entre os voos BH-RIO, BH-SP e BH-BRASILIA.
+    É possível fechar o programa digitando 9.
+
+Parâmetros:
+    Nenhum.
+
+Retorno:
+    int - 0 (indicando que a função foi executada com sucesso).
+****************************** */
 int menuInicial() {
     int vooSelecionado;
     char input[100];
@@ -79,7 +92,19 @@ int menuInicial() {
     return 0;
 }
 
+/* ******************************
+Função: menuVoos
+Descrição:
+    Exibe o menu de opções para um determinado voo selecionado. Permite ao usuário interagir com diferentes funcionalidades do programa,
+    como mostrar a lista de passageiros, pesquisar passageiros por CPF ou nome, cadastrar novos passageiros, excluir passageiros da lista e
+    mostrar a lista de espera. O usuário também pode selecionar outro voo ou sair do programa.
 
+Parâmetros:
+    int vooSelecionado - o índice do voo selecionado.
+
+Retorno:
+    int - 0 (indicando que a função foi executada com sucesso).
+****************************** */
 int menuVoos(int vooSelecionado) {
     Passageiro listaDePassageiros[MAXIMODEPASSAGEIROS];
     int quantidadeDePassageiros = 0;
@@ -144,7 +169,25 @@ int menuVoos(int vooSelecionado) {
     return 0;
 }
 
+/* ******************************
+Função: carregaBancoDeDadosDoVooSelecionado
+Descrição:
+    Carrega os dados do banco de dados de passageiros correspondente ao voo selecionado.
+    Os dados são lidos de um arquivo de texto específico para cada voo e são armazenados na lista de passageiros fornecida.
 
+Parâmetros:
+    int vooSelecionado - o índice do voo selecionado.
+    Passageiro *listaDePassageiros - um ponteiro para a lista de passageiros onde os dados serão armazenados.
+
+Retorno:
+    int - a quantidade de passageiros carregados do banco de dados.
+
+Observações:
+    - A função espera que existam arquivos de texto nomeados "BHRIO.txt", "BHSP.txt" e "BHBRASILIA.txt" correspondendo aos voos BH-RIO, BH-SP e BH-BRASILIA, respectivamente.
+    - A função assume que a estrutura de dados Passageiro está corretamente definida e que a
+    listaDePassageiros tem capacidade suficiente para armazenar todos os passageiros do banco de dados.
+    - A função retorna 0 se ocorrer algum erro ao carregar os dados do voo selecionado.
+****************************** */
 int carregaBancoDeDadosDoVooSelecionado(int vooSelecionado, Passageiro *listaDePassageiros) {
     FILE *bancoDeDados;
     switch (vooSelecionado) {
@@ -199,7 +242,20 @@ int carregaBancoDeDadosDoVooSelecionado(int vooSelecionado, Passageiro *listaDeP
     return quantidadeDePassageiros;
 }
 
+/* ******************************
+Função: mostrarListaDePassageiros
+Descrição:
+    Mostra a lista de passageiros do voo.
+    A função exibe o nome, CPF, número da passagem e número da poltrona de cada passageiro.
+    Caso a quantidade de passageiros seja maior que 10, apenas os primeiros 10 passageiros serão exibidos.
 
+Parâmetros:
+    Passageiro *listaDePassageiros - um ponteiro para a lista de passageiros.
+    int quantidadeDePassageiros - a quantidade total de passageiros.
+
+Retorno:
+    int - 0 (indicando que a função foi executada com sucesso).
+****************************** */
 int mostrarListaDePassageiros(Passageiro *listaDePassageiros, int quantidadeDePassageiros){
     if(quantidadeDePassageiros<=10){
         for(int i = 0; i<quantidadeDePassageiros; i++){
@@ -216,6 +272,24 @@ int mostrarListaDePassageiros(Passageiro *listaDePassageiros, int quantidadeDePa
     return 0;
 }
 
+/* ******************************
+Função: buscarPassageiroPorNome
+Descrição:
+    Busca por um passageiro no voo pelo seu nome.
+    A função recebe como input do usuário uma string e realiza a busca na lista de passageiros do voo.
+    Caso o passageiro seja encontrado, são exibidos nome, CPF, telefone, número da passagem, número da poltrona, número do voo e horário do voo.
+    Caso o passageiro não seja encontrado, uma mensagem informando isso é exibida.
+
+Parâmetros:
+    Passageiro *listaDePassageiros - um ponteiro para a lista de passageiros.
+    int quantidadeDePassageiros - a quantidade total de passageiros.
+
+Retorno:
+    int - 0 (indicando que a função foi executada com sucesso).
+Observações:
+    - Caso o nome digitado pelo usuário seja ambíguo e possa se referir a mais de um passageiro, a função exibirá os detalhes de todos os passageiros encontrados com nomes semelhantes.
+    - A função considera a comparação de nomes em letras minúsculas, ignorando diferenças de caixa (maiúsculas ou minúsculas). Portanto, a busca não é sensível a maiúsculas ou minúsculas.
+****************************** */
 int buscarPassageiroPorNome(Passageiro *listaDePassageiros, int quantidadeDePassageiros){
     char inputNome[100];
     getchar();
@@ -225,14 +299,13 @@ int buscarPassageiroPorNome(Passageiro *listaDePassageiros, int quantidadeDePass
     int passageiroNoVoo = 0;
     for(int i = 0; inputNome[i]; i++){
       inputNome[i] = tolower(inputNome[i]);
-      if(inputNome[i] == ' ')inputNome[i] = '\0';
     }
     char passageiroAtual[100];
     for(int i = 0; i<quantidadeDePassageiros; i++){
         strcpy(passageiroAtual, listaDePassageiros[i].nome);
+        passageiroAtual[strlen(inputNome)] = '\0';
         for(int i = 0; passageiroAtual[i]; i++){
           passageiroAtual[i] = tolower(passageiroAtual[i]);
-          if(passageiroAtual[i] == ' ')passageiroAtual[i] = '\0';
         }
         if(strcmp(passageiroAtual, inputNome) == 0){
             printf("Passageiro: %s - CPF: %s - Telefone: %s - Numero da passagem: %d - Numero da poltrona: %d - Numero do voo: %d - Horario do voo: %s\n",
@@ -253,6 +326,26 @@ int buscarPassageiroPorNome(Passageiro *listaDePassageiros, int quantidadeDePass
     return 0;
 }
 
+/* ******************************
+Função: buscarPassageiroPorCPF
+Descrição:
+    Busca por um passageiro no voo pelo seu CPF.
+    A função solicita ao usuário que digite o CPF do passageiro e realiza a busca na lista de passageiros do voo.
+    Caso o passageiro seja encontrado, são exibidos nome, CPF, telefone, número da passagem, número da poltrona, número do voo e horário do voo.
+    Caso o passageiro não seja encontrado, uma mensagem informando isso é exibida.
+
+Observações:
+    - A função desconsidera caracteres especiais no CPF fornecido pelo usuário.
+    Isso é feito para facilitar o input do usuário, permitindo que ele digite o CPF com ou sem pontuações.
+    Por exemplo, se o CPF do passageiro for "123.456.789-00", o usuário pode digitar "12345678900" e o sistema realizará a busca corretamente.
+
+Parâmetros:
+    Passageiro *listaDePassageiros - um ponteiro para a lista de passageiros.
+    int quantidadeDePassageiros - a quantidade total de passageiros.
+
+Retorno:
+    int - 0 (indicando que a função foi executada com sucesso).
+****************************** */
 int buscarPassageiroPorCPF(Passageiro *listaDePassageiros, int quantidadeDePassageiros) {
     char inputCPF[100];
     getchar();
@@ -303,7 +396,21 @@ int buscarPassageiroPorCPF(Passageiro *listaDePassageiros, int quantidadeDePassa
     return 0;
 }
 
+/* ******************************
+Função: mostrarListaDeEspera
+Descrição:
+    Exibe a lista de espera de passageiros de um voo.
+    A função verifica se o número total de passageiros é maior que 10.
+    Caso seja menor ou igual a 10, significa que não há lista de espera e uma mensagem indicando isso é exibida.
+    Caso contrário, a função exibe os detalhes dos passageiros da lista de espera, incluindo nome, CPF, número da passagem e número da poltrona.
 
+Parâmetros:
+    Passageiro *listaDePassageiros - um ponteiro para a lista de passageiros.
+    int quantidadeDePassageiros - a quantidade total de passageiros.
+
+Retorno:
+    int - 0 (indicando que a função foi executada com sucesso).
+****************************** */
 int mostrarListaDeEspera(Passageiro *listaDePassageiros, int quantidadeDePassageiros){
     if(quantidadeDePassageiros<=10){
         printf("Esse voo esta sem lista de espera.\n");
@@ -316,12 +423,26 @@ int mostrarListaDeEspera(Passageiro *listaDePassageiros, int quantidadeDePassage
     return 0;
 }
 
+/* ******************************
+Função: validarCPF
+Descrição:
+    Verifica se um CPF é válido, ou seja, verifica a formatação dele e se é composto apenas por números e os respectivos caracteres especiais.
+    Caso o CPF seja válido, a função retorna 1. Caso contrário, retorna 0.
+
+Observação:
+    -Essa função é utilizada especificamente para o cadastro de passageiros.
+
+Parâmetros:
+    const char *cpf - uma string contendo o CPF a ser validado.
+
+Retorno:
+    int - 1 se o CPF é válido, 0 caso contrário.
+****************************** */
 int validarCPF(const char *cpf) {
     int i;
     int digitos[11];
     int soma = 0;
 
-    // Verifica se o CPF possui 11 dígitos
     if (strlen(cpf) != 14)
         return 0;
 
@@ -344,6 +465,20 @@ int validarCPF(const char *cpf) {
     return 1;
 }
 
+/* ******************************
+Função: cadastrarPassageiro
+Descrição:
+    Realiza o cadastro de um passageiro, recebendo as informações necessárias e armazenando no banco de dados correspondente ao voo selecionado.
+    A função também realiza algumas validações, como verificar se o CPF já está cadastrado e se a fila de passageiros está cheia.
+
+Parâmetros:
+    Passageiro *listaDePassageiros - um array de estruturas Passageiro que armazena os dados dos passageiros.
+    int *quantidadeDePassageiros - um ponteiro para a variável que indica a quantidade atual de passageiros cadastrados.
+    int vooSelecionado - um valor que representa o voo selecionado para o cadastro do passageiro.
+
+Retorno:
+    int - 0 se o cadastro for realizado com sucesso, 1 caso contrário.
+****************************** */
 int cadastrarPassageiro(Passageiro *listaDePassageiros, int *quantidadeDePassageiros, int vooSelecionado) {
     FILE *bancoDeDados;
     switch (vooSelecionado) {
@@ -458,6 +593,28 @@ int cadastrarPassageiro(Passageiro *listaDePassageiros, int *quantidadeDePassage
     return 0;
 }
 
+/* ******************************
+Função: excluirPassageiro
+Descrição:
+    Exclui um passageiro da lista de passageiros do voo selecionado.
+    A função recebe o ID do passageiro a ser excluído e realiza a exclusão da lista, atualizando o arquivo de banco de dados do voo.
+
+Parâmetros:
+    Passageiro listaDePassageiros[] - um array de estruturas Passageiro contendo a lista de passageiros do voo.
+    int *quantidadeDePassageiros - um ponteiro para a quantidade total de passageiros.
+    int vooSelecionado - um inteiro indicando o voo selecionado.
+
+Retorno:
+    int - 0 (indicando que a função foi executada com sucesso).
+Observações:
+    - A função busca o passageiro pelo ID informado e remove o seu registro da lista de passageiros, reescrevendo todos os registros restantes na nova ordem.
+    - Caso o ID do passageiro esteja dentro do intervalo de 1 a 9 (inclusivo) e a quantidade de passageiros seja maior que 10,
+    é exibida uma mensagem informando que um passageiro saiu da lista de espera e entrou para o voo.
+    - A função utiliza arquivos de texto para armazenar os dados do voo. Os nomes dos arquivos são formados de acordo com o voo selecionado: "BHRIO.txt", "BHSP.txt" e "BHBRASILIA.txt".
+    - Caso não seja possível abrir ou carregar os dados do banco de dados do voo selecionado, a função exibe uma mensagem de erro e retorna 1.
+    - Os dados dos passageiros são armazenados no arquivo de banco de dados no seguinte formato:
+    "id: %d| cpf: %s| nome: %s| endereco: %s| telefone: %s| numero da passagem: %d| numero da poltrona: %d| numero do voo: %d| horario: %s\n"
+****************************** */
 int excluirPassageiro(Passageiro listaDePassageiros[], int *quantidadeDePassageiros, int vooSelecionado) {
     FILE *bancoDeDados;
     switch (vooSelecionado) {
@@ -544,7 +701,7 @@ int excluirPassageiro(Passageiro listaDePassageiros[], int *quantidadeDePassagei
     }
 
     for (int i = 0; i < *quantidadeDePassageiros; i++) {
-        fprintf(bancoDeDados, "id: %d | cpf: %s| nome: %s| endereco: %s | telefone: %s | numero da passagem: %d | numero da poltrona: %d | numero do voo: %d | horario: %s\n",
+        fprintf(bancoDeDados, "id: %d| cpf: %s| nome: %s| endereco: %s| telefone: %s| numero da passagem: %d| numero da poltrona: %d| numero do voo: %d| horario: %s\n",
                 i+1, listaDePassageiros[i].cpf, listaDePassageiros[i].nome, listaDePassageiros[i].endereco,
                 listaDePassageiros[i].telefone, listaDePassageiros[i].numeroPassagem, listaDePassageiros[i].numeroPoltrona,
                 listaDePassageiros[i].numeroVoo, listaDePassageiros[i].horarioVoo);
